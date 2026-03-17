1b1192930b493152a73fc48d47a3046f0b89443f
Annie Berjou <annie.carrau@wanadoo.fr>
2026-03-17 16:52:33 +0100

    trier le slider dans l'ordre décroissant

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -13,8 +13,8 @@ const generateKey = () => {
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1,
+  const byDateDesc = data?.focus.sort(
+    (evtA, evtB) => new Date(evtB.date) - new Date(evtA.date),
   );
   const nextCard = () => {
     setTimeout(() => setIndex(index < byDateDesc.length ? index + 1 : 0), 5000);
